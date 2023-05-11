4fee5f4ad3a60b1f51ed118b003dd30192b9f195
eunji Lee <eunjitech@gmail.com>
2023-05-11 19:14:26 +0900

    2-15: styling

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{
+          borderWidth: 1,
+          borderColor: "#000",
+          padding: 10,
+        }}
+      >
+        Hello World!!!
+      </Text>
       <StatusBar style="auto" />
+      <Button title="Click Me" />
     </View>
   );
 }
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
